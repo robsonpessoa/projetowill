02a20f2e6365993514b2b3d53347ca35eb320f25
robsonpessoa <robson.pessoa@fpf.br>
2020-03-30 18:35:48 -0300

    Refactoring getEpisode privado

--- src/Series.java
+++ src/Series.java
@@ -1,24 +1,31 @@
 import java.util.ArrayList;
-public class Series extends Media{
+
+public class Series extends Media {
     private ArrayList<ArrayList<Episode>> episodes;
 
-    public Series(String name, Util.ageRatingsEnum ageRating, ArrayList<Util.genresEnum> genres){
-        super(name, ageRating,  genres);
+    public Series(String name, Util.ageRatingsEnum ageRating, ArrayList<Util.genresEnum> genres) {
+        super(name, ageRating, genres);
         this.episodes = new ArrayList<ArrayList<Episode>>();
     }
-    public void incrementViews(int episode, int season){
-    
-    if(season> this.episodes.size()){
-        System.out.println("O número da temporada digitada é invalido");
-    }
-        ArrayList<Episode> _season = this.episodes.get(season-1);
-        if(episode<0 || episode > _season.size() ){
-            System.out.println("O número do episódio que digitou é invalido");
-        }
-        Episode ep=_season.get(episode);
+
+    public void incrementViews(int episode, int season) {
+        Episode ep = getEpisode(episode, season);
         ep.incrementView();
     }
-    public void addUserRating(float _userRating, int episode, int season){
 
+    public void addUserRating(float _userRating, int episode, int season) {
+        Episode ep = getEpisode(episode, season);
+        ep.addUserRating(_userRating);
+    }
+
+    private Episode getEpisode(int episode, int season) {
+        if (season > this.episodes.size()) {
+            System.out.println("O número da temporada digitada é invalido");
+        }
+        ArrayList<Episode> _season = this.episodes.get(season - 1);
+        if (episode < 0 || episode > _season.size()) {
+            System.out.println("O número do episódio que digitou é invalido");
+        }
+        return _season.get(episode);
     }
 }
